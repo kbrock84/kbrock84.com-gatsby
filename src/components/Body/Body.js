44a19da1270a5dbd41b08967767bde5f0efd9fee
Kevin Brock <kevin.brock@hotmail.com>
2019-03-12 05:35:36 -0700

    simplified Body and use hooks

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,32 +1,19 @@
 import React from "react";
-import { Component } from "react";
+import { useContext } from "react";
 import BodyWrapper from "./BodyWrapper";
 import MainMenu from "../MainMenu/MainMenu";
 import ContentContainer from "../ContentContainer/ContentContainer";
 import { PageContext } from "../../PageContext/PageContext";
 
-class Body extends Component {
-  constructor(props) {
-    super(props);
-    this.throttled = false;
-  }
+const Body = props => {
+  const context = useContext(PageContext);
 
-  static contextType = PageContext;
-
-  render() {
-    return (
-      <>
-        <BodyWrapper mobile={this.context.mobile}>
-          <>
-            <MainMenu width={268} menuItems={this.props.categories} />
-            <ContentContainer title={this.props.title}>
-              {this.props.children}
-            </ContentContainer>
-          </>
-        </BodyWrapper>
-      </>
-    );
-  }
-}
+  return (
+    <BodyWrapper mobile={context.mobile}>
+      <MainMenu width={268} menuItems={props.categories} />
+      <ContentContainer title={props.title}>{props.children}</ContentContainer>
+    </BodyWrapper>
+  );
+};
 
 export default Body;
