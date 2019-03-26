87a8e61329f39479aa502d90e1ab99aa865a6f94
Kevin Brock <kevin.brock@hotmail.com>
2019-03-26 06:37:15 -0700

    fixed margin on main menu

--- src/components/Body/BodyWrapper.js
+++ src/components/Body/BodyWrapper.js
@@ -6,7 +6,7 @@ const BodyWrapper = styled.div`
   width: 100vw;
   grid-template-columns: auto;
   grid-gap: ${props => (props.isHome ? "32px" : "0")};
-  ${props => (props.mobile ? "padding-top: 78px;" : "padding-left: 78px")}
+  ${props => (props.mobile ? "padding-top: 70px;" : "padding-left: 78px")}
 `;
 
 export default BodyWrapper;
