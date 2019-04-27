d599ca88c3978c952c3a6a04bf6ef0f3df86e231
Kevin Brock <kevin.brock@hotmail.com>
2019-04-27 07:48:53 -0700

    Jest setup and simple snapshot of CircularText.js

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+    "^.+\\.jsx?$": `<rootDir>/jest-preprocess.js`
+  },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.([tj]sx?)$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  moduleNameMapper: {
+    ".+\\.(css|styl|less|sass|scss)$": `identity-obj-proxy`,
+    ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `<rootDir>/__mocks__/file-mock.js`
+  },
+  testPathIgnorePatterns: [`node_modules`, `.cache`],
+  transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
+  globals: {
+    __PATH_PREFIX__: ``
+  },
+  testURL: `http://localhost`,
+  setupFiles: [`<rootDir>/loadershim.js`],
+  setupFilesAfterEnv: [`<rootDir>/jest.setup.js`]
+};
